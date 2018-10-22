1f18f6d996a90e0a0be0cbe7bbcfef328c59b1b8
Ankit Sharma <39335141+AnkitSharma19@users.noreply.github.com>
2018-10-22 04:33:08 +0100

    v1

--- src/logexception/logframework.py
+++ src/logexception/logframework.py
@@ -6,34 +6,61 @@ Create a logging framework to collect all the logs into a single file .Please fo
  - The logging format has to be generic with the module_name, function_name, line_no : message
 '''
 
-from functools import wraps
+# from functools import wraps
 
-class customlogging:
-    def __init__(self):
-        self.name = ''
-        self.time = ''
-        self.filename = ''
-        self.functionname = ''
+import logging.config
+import logging
 
-    def my_logging(orig_func):
-        import logging
-        logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
-        @wraps(orig_func)
-        def wrapper(*args, **kwargs):
-            logging.info('Ran with args: {}, and kwargs: {}'.format(args, kwargs) )
-            return orig_func(*args, **kwargs)
-        return wrapper
+class CustomLogging:
+    def ApplicationExceptionLog(self):
+        logger = logging.getLogger(__name__)
+        logger.info('Application Exception')
+
+    def ValueErrorLog(self):
+        logger = logging.getLogger(__name__)
+        logger.info('Value Exception Log')
+
+    def TypeErrorLog(self):
+        logger = logging.getLogger(__name__)
+        logger.info('Value Exception Log')
 
 
-    # Logging time function
-    def my_timer(orig_func):
-        import time
-        @wraps(orig_func)
-        def wrapper(*args, **kwargs):
-            t1 = time.time()
-            result = orig_func(*args, **kwargs)
-            t2 = time.time() - t1
-            print('{} ran in: {} sec\n\n'.format(orig_func.__name__, t2))
-            return result
+    class ExceptionLog(object):
+        def __init__(self, logger=None):
+            self.logger = logger or logging.getLogger(__name__)
 
-        return wrapper
+        def ExceptionLog(self):
+            self.logger.info('Exception Log')
+
+    # custom = yaml.load(open('config.yaml', 'r'))
+    # logging.config.dictConfig()
+
+
+
+    # def __init__(self, name, time, filename,functionname):
+    #     self.name = ''
+    #     self.time = ''
+    #     self.filename = ''
+    #     self.functionname = ''
+    #
+    # def my_logging(orig_func):
+    #     import logging
+    #     logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+    #     @wraps(orig_func)
+    #     def wrapper(*args, **kwargs):
+    #         logging.info('Ran with args: {}, and kwargs: {}'.format(args, kwargs) )
+    #         return orig_func(*args, **kwargs)
+    #     return wrapper
+    #
+    #
+    # # Logging time function
+    # def my_timer(orig_func):
+    #     import time
+    #     @wraps(orig_func)
+    #     def wrapper(*args, **kwargs):
+    #         t1 = time.time()
+    #         result = orig_func(*args, **kwargs)
+    #         t2 = time.time() - t1
+    #         print('{} ran in: {} sec\n\n'.format(orig_func.__name__, t2))
+    #         return result
+    #     return wrapper
--- src/logexception/parsecsv.py
+++ src/logexception/parsecsv.py
@@ -1,10 +1,20 @@
 # Error & Exception handling
 
 from src.logexception.exceptionhandler import ValueErrorException
-import src.logexception.logframework
+import logging
+from src.logexception.logframework import CustomLogging
+import yaml
 
-
+# @my_logging
+# @my_timer
 def parse_csv_and_get_columns(filename):
+
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        logger = logging.getLogger(__name__)
+        # logging.debug('error message')
+
     count = 0
     try:
         csvFile = open(filename, 'r')
@@ -14,15 +24,13 @@ def parse_csv_and_get_columns(filename):
             test_str_div = val[0] / val[11]
 
     except IOError:
-        print('IO Error | An error occurred trying to read the file.')
+        CustomLogging.ApplicationExceptionLog('IO Error | An error occurred trying to read the file.')
 
     except ValueErrorException:
-        print('ValueErrorException | Non-numeric data found in the file.')
+        CustomLogging.ValueErrorLog('ValueErrorException | Non-numeric data found in the file.')
 
     except TypeError:
-        print('TypeErrorException | Please check the operands type')
-
-
+        CustomLogging.TypeErrorLog('TypeErrorException | Please check the operands type')
 
 if __name__ == "__main__":
     parse_csv_and_get_columns(filename="data/flights.csv")
